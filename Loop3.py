542b0f619f430578654218b6e094f52300912c10
Sanchita-b-20 <bobdesanchita20@gmail.com>
2026-03-13 17:21:51 +0530

    Loop3.py

---
+++ Loop3.py
@@ -0,0 +1,57 @@
+# for i in range(1,5):
+#     if i==3:
+#         break
+#     print(i)
+
+#_______________________________________________________________________________________________________
+
+# for i in range(1,5):
+#     if i==3:
+#         continue
+#     print(i)
+
+#_______________________________________________________________________________________________________
+
+# for i,j in zip(range(1,6),range(5,0,-1)):
+#     if i==3 and j==3:
+#         continue
+#     print(i," ",j)
+#palindrome
+
+# name=input("enter a name:")
+# newname=name[::-1]
+# if name==newname:
+#     print("palindrome")
+# else:
+#     print("not a palindrome")
+
+#______________________________________________________________________________________________
+
+# #anagram - check if both words have same and equal letters like listen=silent
+# a=input("enter word 1:")
+# b=input("enter word 2:")
+# #do by sorting
+
+#_______________________________________________________________________________________________
+
+#adding key value pair to a dictionary
+# my_dict = {}
+# my_dict['name'] = 'Sanchita'
+# my_dict['age'] = 20
+# print("After adding key-value pairs:", my_dict)
+
+#__________________________________________________________________________________________________
+
+#remove a key value pair 
+
+# my_dict = {}
+# my_dict['name'] = 'Sanchita'
+# my_dict['age'] = 20
+# print("After adding key-value pairs:", my_dict)
+
+#___________________________________________________________________________________________________
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(i, end=" ")
+#     print()
